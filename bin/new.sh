8e6ad48dfa838ee8fb09dc354731c3f4a71080cf
Frank Quednau <fquednau@gmail.com>
2016-12-18 23:52:58 +0100

    admin changes

--- bin/new.sh
+++ bin/new.sh
@@ -1,4 +1,6 @@
 # create a new post
+todayFile=$(date +"%Y-%m-%d")
+todayContent=$(date +"%Y-%m-%d %T")
 
 printf "Title: "
 read -r title
@@ -10,15 +12,16 @@ titleForFile=$(echo "$title" |
 	sed 's/[ ?<>*=!'\'']/-/g' | 
 	sed -E 's/-+/-/g' | sed -E 's/-$//g')
 
-cat >_drafts/date-$titleForFile.md <<EOL
+cat >../_posts/$todayFile-$titleForFile.md <<EOL
 ---
 title: "$title"
 layout: post
 tags: [$tags]
-date: date-placeholder
+date: $todayContent
 ---
+
 EOL
 
-echo "_drafts/date-$titleForFile.md" | pbcopy
+echo "../_posts/$todayFile-$titleForFile.md" | pbcopy
 echo "created $titleForFile and put path to clipboard"
 
