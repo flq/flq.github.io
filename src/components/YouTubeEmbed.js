468cf3cc5900cf78d860a85d4a0d56340b124927
Frank Quednau <fquednau@gmail.com>
2021-05-12 23:46:35 +0200

    more work, work, work, mostly date

--- src/components/YouTubeEmbed.js
+++ src/components/YouTubeEmbed.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import * as styles from "./YouTubeEmbed.module.css"
 
 export function YouTubeEmbed({ link }) {
     return (<iframe 
         title={`Youtube embed for ${link}`}
-        width="560" 
-        height="315" 
+        className={styles.frame}
         src={link} 
         frameborder="0" 
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
--- src/components/postExcerpt.js
+++ src/components/postExcerpt.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link } from 'gatsby'
 import * as styles from './postExcerpt.module.css'
 
 export default function PostExcerpt({ slug, title, date, excerpt }) {
   return (
     <article className={styles.article}>
-      <h2 className={styles.header}>
-        <Link to={slug}>{title}</Link>
-      </h2>
-      <time className={styles.date} datetime={date}>{date}</time>
+      <header className={styles.headerContainer}>
+        <DateDisplay dateStr={date} />
+        <h2 className={styles.header}>
+          <Link to={slug}>{title}</Link>
+        </h2>
+      </header>
       <p className={styles.excerpt}>{excerpt}</p>
     </article>
+  )
+}
+
+function DateDisplay({ dateStr }) {
+  const [date, month, year] = useMemo(() => {
+    const d = new Date(dateStr)
+    return [
+      d.getDate(),
+      d.toLocaleString('en', { month: 'short' }),
+      d.getFullYear(),
+    ]
+  }, [dateStr])
+  return (
+    <time className={styles.dateContainer} datetime={dateStr}>
+      <span className={styles.month}>{month}</span>
+      <span className={styles.date}>{date}</span>
+      <span className={styles.year}>{year}</span>
+    </time>
   )
 }
