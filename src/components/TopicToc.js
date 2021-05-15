230d6ca54a3b820e877e89862698c5a0f26269b9
Frank Quednau <fquednau@gmail.com>
2021-05-15 21:01:49 +0200

    tweet blockquote topic toc

--- src/components/TopicToc.js
+++ src/components/TopicToc.js
@@ -1,47 +1,56 @@
-import React from "react"
+import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { useLocation } from '@reach/router';
+import { useLocation } from '@reach/router'
+import * as styles from './TopicToc.module.css'
 
 export function TopicToc({ header, topicId }) {
-
-    const location = useLocation()
-    const { allMdx: { edges } } = useStaticQuery(graphql`query PagesWithTopic {
-        allMdx(sort: {
-          order: ASC, 
-          fields: [frontmatter___date]}, 
-          filter: {
-            fields: {published: {eq: true}},
-            frontmatter: {topic: {ne: null}}
-          }) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                topic
-                title
-              }
+  const location = useLocation()
+  const {
+    allMdx: { edges },
+  } = useStaticQuery(graphql`
+    query PagesWithTopic {
+      allMdx(
+        sort: { order: ASC, fields: [frontmatter___date] }
+        filter: {
+          fields: { published: { eq: true } }
+          frontmatter: { topic: { ne: null } }
+        }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              topic
+              title
             }
           }
         }
-      }`);
+      }
+    }
+  `)
 
-    return (
-        <>
-            <h3 className="seriestoc">{header}</h3>
-            <ol className="seriestoc">
-                {edges
-                    .filter(e => e.node.frontmatter.topic === topicId)
-                    .map(({ node: { fields, frontmatter } }) => {
-                        return (<li key={fields.slug}>
-                            {location.pathname.toLowerCase().startsWith(fields.slug.toLowerCase()) ?
-                                <span>{frontmatter.title}</span> :
-                                <a href={fields.slug}>{frontmatter.title}</a>
-                            }
-                        </li>)
-                    })}
-            </ol>
-        </>
-    )
+  return (
+    <nav className={styles.container}>
+      <h3 className={styles.header}>{header}</h3>
+      <ol className={styles.list}>
+        {edges
+          .filter((e) => e.node.frontmatter.topic === topicId)
+          .map(({ node: { fields, frontmatter } }) => {
+            return (
+              <li key={fields.slug}>
+                {location.pathname
+                  .toLowerCase()
+                  .startsWith(fields.slug.toLowerCase()) ? (
+                  <span>{frontmatter.title}</span>
+                ) : (
+                  <a href={fields.slug}>{frontmatter.title}</a>
+                )}
+              </li>
+            )
+          })}
+      </ol>
+    </nav>
+  )
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import * as styles from './Tweet.module.css'
 
 export function Tweet({ tweet }) {
-    const theTweet = { __html: tweet }
-    return (<div dangerouslySetInnerHTML={theTweet}></div>)
+  const theTweet = { __html: tweet }
+  return <div className={styles.tweet} dangerouslySetInnerHTML={theTweet}></div>
 }
