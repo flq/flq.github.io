41ee0f4616f615b10a6782244344fdba8dd30e0a
Frank Quednau <fquednau@gmail.com>
2021-05-16 16:37:32 +0200

    rescued hesitan miracles css

--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import * as styles from './Discussion.module.css'
 
 export function Alpha({ children }) {
-  return <p className="alpha">{children}</p>
+  return <p className={styles.alpha}>{children}</p>
 }
 
 export function Beta({ children }) {
-    return <p className="beta">{children}</p>
-  }
+  return <p className={styles.beta}>{children}</p>
+}
