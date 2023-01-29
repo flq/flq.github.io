e2d68a870829f938f8aa12c13ba6d863968f79fc
Frank Quednau <fquednau@gmail.com>
2023-01-29 14:33:12 +0100

    Infra changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import { extractExcerptPlugin } from './support/extract-excerpt.mjs';
-// import react from "@astrojs/react";
 
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [extractExcerptPlugin],
-    extendDefaultPlugins: true,
-    syntaxHighlight: 'prism'
-  },
   integrations: [
-    mdx()
+    mdx({
+      remarkPlugins: [extractExcerptPlugin],
+      syntaxHighlight: 'prism'
+    }),
+    react()
   ]
 });
