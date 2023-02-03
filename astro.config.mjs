5867c2ba190efb0e61ed591ef4991748c7f24011
Frank Quednau <fquednau@gmail.com>
2023-02-03 20:00:06 +0100

    added site.xml support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import { extractExcerptPlugin } from './support/extract-excerpt.mjs';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx({
-      remarkPlugins: [extractExcerptPlugin],
-      syntaxHighlight: 'prism'
-    }),
-    react()
-  ]
+  site: "https://realfiction.net",
+  integrations: [mdx({
+    remarkPlugins: [extractExcerptPlugin],
+    syntaxHighlight: 'prism'
+  }),
+  sitemap()]
 });
