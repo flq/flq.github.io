87dca47dd91e3d684fa2bb2028b523078d99619b
Frank Quednau <fquednau@gmail.com>
2024-05-01 17:47:40 +0200

    new code visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { extractExcerptPlugin } from './support/extract-excerpt.mjs';
+import { extractExcerptPlugin } from "./support/extract-excerpt.mjs";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://realfiction.net",
-  integrations: [mdx({
-    remarkPlugins: [extractExcerptPlugin],
-    syntaxHighlight: 'prism'
-  }),
-  sitemap()]
+  integrations: [
+    expressiveCode({ themes: ["dracula"] }),
+    mdx({
+      remarkPlugins: [extractExcerptPlugin],
+    }),
+    sitemap(),
+  ],
 });
