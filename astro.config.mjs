17cd33034c417eb992ab20da387015750e07b734
Frank Quednau <fquednau@gmail.com>
2025-03-19 23:01:38 +0100

    Enable mermaid usage in the mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { extractExcerptPlugin } from "./support/extract-excerpt.mjs";
+import rehypeMermaid from "rehype-mermaid";
 
 import expressiveCode from "astro-expressive-code";
 
@@ -20,7 +21,11 @@ export default defineConfig({
   integrations: [
     expressiveCode({ themes: ["dracula"] }),
     mdx({
+      syntaxHighlight: {
+        excludeLangs: ['mermaid']
+      },
       remarkPlugins: [extractExcerptPlugin],
+      rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true, colorScheme: "forest" }]]
     }),
     sitemap(),
   ],
